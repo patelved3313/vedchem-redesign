2aa06a369bd78a9b41d20fd8fdbea9877bc0f7cd
patelved3313 <patelved3313@gmail.com>
2026-04-25 00:46:29 -0400

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,3 +1,16 @@
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
-export default function LocaleLayout({children}:{children:React.ReactNode}){return <><Navbar />{children}<Footer /></>}
+
+export default function LocaleLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+}
